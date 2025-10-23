633293ae83b8d438c29ff944a4b289515cb90dd0
Alena Alekseeva <a77993519@gmail.com>
2025-10-23 15:43:29 +0400

    feat: add function stream in read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const stream = fs.createReadStream(fileToReadPath, 'utf-8');
+  stream.pipe(process.stdout)
 };
 
 await read();
