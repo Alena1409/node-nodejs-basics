d4cda643ed38f9375da93a674357c3dbfb078eb6
Alena Alekseeva <a77993519@gmail.com>
2025-10-26 12:45:13 +0400

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import { Worker } from 'node:worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const performCalculations = async () => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const workerPath = path.join(__dirname, 'worker.js');
+
+  const cores = os.cpus().length;
+
+  const startNumber = 10;
+  const workers = [];
+
+  for (let i = 0; i < cores; i++) {
+    const n = startNumber + i;
+
+    const promise  = new Promise((resolve) => {
+      const worker = new Worker(workerPath);
+
+      worker.postMessage(n);
+
+      worker.on('message', (result) => {
+        resolve({ status: 'resolved', data: result });
+        worker.terminate();
+      });
+
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+        worker.terminate();
+      });
+
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          resolve({ status: 'error', data: null });
+        }
+      });
+    });
+
+    workers.push(promise);
+  }
+
+  const results = await Promise.all(workers);
+  console.log(results);
 };
 
 await performCalculations();
