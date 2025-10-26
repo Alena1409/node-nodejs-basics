402af9fa53ee49c653d178e22edbd140d826f438
Alena Alekseeva <a77993519@gmail.com>
2025-10-26 13:10:40 +0400

    feat: add function spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const scriptPath = path.join(__dirname, 'files', 'script.js');
+  const child = spawn('node', [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
+
+  child.stderr.on('data', (data) => {
+    console.error(`Child Process Error: ${data.toString()}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--user', 'Name']);
